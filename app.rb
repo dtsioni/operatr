7c2c6a4a749eecb94857d21e379bdc394a792721
Daniel Tsioni <dtsioni@gmail.com>
2017-01-01 17:49:20 -0500

    Initial commit

---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'twilio-ruby'
+require 'dotenv'
+Dotenv.load
+twilio_auth_token = ENV["TWILIO_AUTH_TOKEN"]
+twilio_account_sid = ENV["TWILIO_ACCOUNT_SID"]
+
+@client = Twilio::REST::Client.new twilio_account_sid, twilio_auth_token
+
+@client.messages.create(
+  from: '+18622775064',
+  to: '+19738653323',
+  body: 'Hey there!'
+)
+
