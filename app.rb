b070755fd948216a066556fdecdf30140d9ce6a4
Daniel Tsioni <dtsioni@gmail.com>
2017-01-01 17:55:33 -0500

    Set up sinatra app

--- app.rb
+++ app.rb
@@ -1,14 +1,20 @@
 require 'twilio-ruby'
 require 'dotenv'
+require 'sinatra'
+
 Dotenv.load
 twilio_auth_token = ENV["TWILIO_AUTH_TOKEN"]
 twilio_account_sid = ENV["TWILIO_ACCOUNT_SID"]
 
 @client = Twilio::REST::Client.new twilio_account_sid, twilio_auth_token
 
-@client.messages.create(
-  from: '+18622775064',
-  to: '+19738653323',
-  body: 'Hey there!'
-)
+#@client.messages.create(
+ # from: '+18622775064',
+  #to: '+19738653323',
+ # body: 'Hey there!'
+#)
+
+get '/' do
+  'hello world'
+end
 
