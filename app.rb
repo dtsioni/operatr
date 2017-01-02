6c17d4405d9a2f0fa4783c214d35da422eb07fe5
Daniel Tsioni <dtsioni@gmail.com>
2017-01-02 13:52:32 -0500

    comments

--- app.rb
+++ app.rb
@@ -9,13 +9,7 @@ require('twilio-ruby')
 twilio_auth_token = ENV["TWILIO_AUTH_TOKEN"]
 twilio_account_sid = ENV["TWILIO_ACCOUNT_SID"]
 
-@client = Twilio::REST::Client.new twilio_account_sid, twilio_auth_token
-
-#@client.messages.create(
- # from: '+18622775064',
-  #to: '+19738653323',
- # body: 'Hey there!'
-#)
+#@client = Twilio::REST::Client.new twilio_account_sid, twilio_auth_token
 
 post '/sms' do
   content_type 'text/xml'
